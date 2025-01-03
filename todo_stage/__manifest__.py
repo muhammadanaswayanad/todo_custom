a1210f41047600cead950c063e2982b140d87494
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-03 10:28:08 +0530

    remove todo_stage module and all associated files

--- todo_stage/__manifest__.py
+++ todo_stage/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Module to set stages for Todo module',
     'description': 'This module sets fixed stages for the Todo module.',
     'author': 'Your Name',
-    'depends': ['base', 'todo_custom'],  # Ensure 'todo_custom' does not depend on 'todo_stage'
+    'depends': ['base', 'todo_custom'],
     'data': [
         'data/todo_stage_data.xml',
         'views/todo_stage_views.xml',
